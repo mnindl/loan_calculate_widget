8cbc9f2b836a080a13af04d5010e448bae0d4553
mnindl <mnindl75@googlemail.com>
2011-09-12 16:18:50 +0200

    fix some bugs, add constants

--- js/loan_calculate_widget.js
+++ js/loan_calculate_widget.js
@@ -32,7 +32,9 @@ LOANCALCULATEWIDGET.compute = (function () {
         "80" : "0,0359",
         "100": "0,039"
     }
-  };
+  },
+  start_buy_price = 640000,
+  start_own_capital = 210000;
 
   function getFormatPrice(price, euro_sign) {
     var temp;
@@ -80,14 +82,14 @@ LOANCALCULATEWIDGET.compute = (function () {
         own_capital_num = Number(own_capital.slice(0, own_capital.length-1)),
         loan_sum = buy_price_num-own_capital_num,
         loan_ratio = ((loan_sum/buy_price_num).toFixed(2))*100,
-        debit_interest = getDebitInterest(loan_sum, loan_ratio);
+        debit_interest = getDebitInterest(loan_sum, loan_ratio),
         monthly_rate = (loan_sum * ((debit_interest + 0.01)/12)).toFixed(0);
     $('#loan_value').html(getFormatPrice(loan_sum, true));
     $('#monthly_value').html(getFormatPrice(monthly_rate, true));
   }
   function setSliderValues(slider, ui) {
-    var own_capital_value = $(".slider_own_capital").slider('value');
-    var buy_price_value =  $(".slider_buy_price").slider('value');
+    var own_capital_value = $(".slider_own_capital").slider('value'),
+        buy_price_value =  $(".slider_buy_price").slider('value');
     if (slider === "buy_price") {
       if (own_capital_value >= ui.value) {
         $('#own_capital_slider .ui-slider-handle').html(getFormatPrice(ui.value, true));
@@ -122,7 +124,7 @@ LOANCALCULATEWIDGET.compute = (function () {
       change: function (e, ui) {
         setSliderValues("buy_price", ui);
         calcLoan();
-      } 
+      }
     },
       slider_opt_own_capital = {
         min: 10000,
@@ -139,15 +141,15 @@ LOANCALCULATEWIDGET.compute = (function () {
         },
         change: function (e, ui) {
           setSliderValues("own_capital", ui);
-          calcLoan();
+            calcLoan();
         }
       };
       $(".slider_buy_price").slider(slider_opt_buy_price);
       $(".slider_own_capital").slider(slider_opt_own_capital);
       $('#sliders').css('visibility', 'visible');
       $('.slider_buy_price .ui-slider-handle').focus();
-      $(".slider_buy_price").slider("value", 640000);
-      $(".slider_own_capital").slider("value", 210000);
+      $(".slider_buy_price").slider("value", start_buy_price);
+      $(".slider_own_capital").slider("value", start_own_capital);
   }
   function init() {
     initSlider();
